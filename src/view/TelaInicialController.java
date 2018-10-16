54933e160cd8052b40194aece28eb6f84c34ec9f
Igor Lobo <lobo.igor19@gmail.com>
2018-10-16 14:13:45 -0300

    adicionando telas base

--- src/view/TelaInicialController.java
+++ src/view/TelaInicialController.java
@@ -3,10 +3,37 @@ package view;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class TelaInicialController implements Initializable {
+//---------------------------COMPONENTES DA TELA FXML----------------------------------------
+    @FXML
+    private Button btn_iniciarJogo;
 
+    @FXML
+    private Button btn_abrirServidor;
+
+    @FXML
+    private TextField txf_tamanhoDoTabuleiro;
+
+//-------------------------------------------------------------------------------------------
+    
+    
+    @FXML
+    void click_btnAbrirServidor(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_btnIniciarJogo(ActionEvent event) {
+
+    }	
+	
+	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
---
+++ src/view/TelaJogoController.java
@@ -0,0 +1,29 @@
+package view;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+
+public class TelaJogoController implements Initializable{
+//---------------------------COMPONENTES DA TELA FXML----------------------------------------
+    @FXML
+    private AnchorPane pane;
+
+    @FXML
+    private GridPane grid;
+
+    @FXML
+    private Label lb_armasRestantes;
+//--------------------------------------------------------------------------------------------
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
