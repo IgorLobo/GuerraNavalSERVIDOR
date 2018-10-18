db98715ff1fbfde0881d839851b769b478858d46
xHygor <hygordebarross@gmail.com>
2018-10-18 11:01:29 -0300

    Estrutura do projeto

--- src/connection/GerenciadorDeClientes.java
+++ src/connection/GerenciadorDeClientes.java
@@ -11,7 +11,7 @@ import java.net.Socket;
 
 import javax.swing.JOptionPane;
 
-import view.TelaInicialController;
+import controller.TelaInicialController;
 
 public class GerenciadorDeClientes extends Thread{
 
